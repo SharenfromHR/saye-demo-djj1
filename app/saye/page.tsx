893826123ae38f674e2d6459e76350df24a44067
SharenfromHR <danieljerome97@hotmail.com>
2025-11-13 16:00:36 +0000

    Update page.tsx

--- app/saye/page.tsx
+++ app/saye/page.tsx
@@ -1,5 +1,16 @@
-import SAYEPage from "../../components/SAYEPage";
+import SAYEPage from "@/components/SAYEPage";
+import SAYEConfig from "@/components/SAYEConfig";
 
-export default function SayeRoute() {
-  return <SAYEPage />;
+export default function Page() {
+  const handleConfigUpdate = (data) => {
+    // this will later sync into your participant view
+    console.log("CONFIG UPDATED:", data);
+  };
+
+  return (
+    <div className="space-y-10 p-10">
+      <SAYEConfig onUpdate={handleConfigUpdate} />
+      <SAYEPage />
+    </div>
+  );
 }
