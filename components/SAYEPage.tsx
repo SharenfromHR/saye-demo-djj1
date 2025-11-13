491f13a10e1dc9ede49a737fc2100326329c9c1b
SharenfromHR <danieljerome97@hotmail.com>
2025-11-13 12:36:21 +0000

    Update import paths for Card and Button components

--- components/SAYEPage.tsx
+++ components/SAYEPage.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useMemo, useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
 import { Info, ChevronDown } from "lucide-react";
 
 const formatMoney = (n: number, ccy = "GBP") =>
