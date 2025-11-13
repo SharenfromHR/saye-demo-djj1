c8ca91718ceee67c2481c9adfded947895df2d36
SharenfromHR <danieljerome97@hotmail.com>
2025-11-13 16:17:39 +0000

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,4 @@
+"use client";
 
 export function Button({ children, ...props }) {
   return <button {...props} className={(props.className||'') + ' border px-3 py-1 rounded-md'}>{children}</button>;
